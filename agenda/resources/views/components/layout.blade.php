197b2d57ec0d9bdb02d33ab633238de6c1b5c364
EDUARDO SANTOS DA SILVA <eduardo.ssilva36@senacsp.edu.br>
2024-11-26 22:10:21 -0300

    Primeiro commit

---
+++ agenda/resources/views/components/layout.blade.php
@@ -0,0 +1,18 @@
+<!doctype hmtl>
+
+<head lang 'PT-br'>
+    <meta charset = 'utf-8'>
+    <title>{{$titulo}}</title>
+</head>
+
+<body>
+
+    <h1>{{$titulo}}</h1>
+
+    <div>
+        {{$slot}}
+    </div>
+
+</body>
+
+</html>
---
+++ agenda/resources/views/paginas/index.blade.php
@@ -0,0 +1,10 @@
+<x-layout titulo="Página Inicial">
+
+    <p>Bem Vindo!</p>
+
+    <a href="/cadastrar"><button>Cadastrar</button></a>
+    <button>Atualizar</button>
+    <button>Consultar</button>
+    <button>Excluir</button>
+
+</x-layout>
